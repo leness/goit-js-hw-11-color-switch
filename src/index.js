ec4aa17bca07651113ef1d0ed985a77cfd5aa95d
Olena Stadnyk <lenesx@gmail.com>
2020-12-13 13:27:33 +0200

    add hw 01

---
+++ src/index.js
@@ -0,0 +1,36 @@
+const colors = [
+  '#FFFFFF',
+  '#2196F3',
+  '#4CAF50',
+  '#FF9800',
+  '#009688',
+  '#795548',
+];
+
+const refs = {
+  body: document.querySelector('body'),
+  btnStart: document.querySelector('[data-action="start"]'),
+  btnStop: document.querySelector('[data-action="stop"]'),
+};
+
+refs.btnStart.addEventListener('click', startClick);
+refs.btnStop.addEventListener('click', stopClick);
+
+let timerId;
+
+function startClick() {
+  timerId = setInterval(() => {
+    refs.body.style.backgroundColor = colors[randomIntegerFromInterval(0, 5)]
+  }, 1000);
+  refs.btnStart.disabled = true;
+};
+
+function stopClick() {
+ clearInterval(timerId);
+  refs.btnStart.disabled = false;
+}
+ 
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
